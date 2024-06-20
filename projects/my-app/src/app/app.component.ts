08af0bef99827aa7ce445524eccd5fb95188bbf6
Nahari Imane <nahariseomaniak@gmail.com>
2024-06-20 17:35:43 +0100

    header added

--- projects/my-app/src/app/app.component.ts
+++ projects/my-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'chafaf-components';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myApp';
+
+  logoUrl = 'https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp';
+  menuItems: MenuItem[] =  [
+    { label: 'Home', link: '/' },
+    { label: 'About', link: '/about' },
+    { label: 'More', link: '#', subItems: [
+          { label: 'Subitem 1', link: '#' },
+          { label: 'Subitem 2', link: '#' },
+          { label: 'Subitem 3', link: '#' }
+        ]
+    },
+    { label: 'Contact', link: '#' }
+  ];
+  languageFlags = [
+    { url: 'https://cdn.countryflags.com/thumbs/spain/flag-round-250.png', code: 'es' },
+    { url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/United-states_flag_icon_round.svg/768px-United-states_flag_icon_round.svg.png', code: 'en' },
+    { url: 'https://cdn.countryflags.com/thumbs/france/flag-round-250.png', code: 'fr' },
+    { url: 'https://cdn.countryflags.com/thumbs/morocco/flag-round-250.png', code: 'ar' }
+  ];
+
+  onLanguageChange(lang: string) {
+    console.log(`Language changed to: ${lang}`);
+  }
 }
